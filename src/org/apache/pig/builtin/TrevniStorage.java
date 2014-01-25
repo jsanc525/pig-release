11a9fedc90577fc9aeb8f41d5fee3804572b6737
Daniel Dai <daijy@hortonworks.com>
2014-01-24 19:45:54 -0800

    Revert "PIG-3558: ORC support for Pig"
    This reverts commit 04c567fe70a482630de570ba74708923c701d6e4.

--- src/org/apache/pig/builtin/TrevniStorage.java
+++ src/org/apache/pig/builtin/TrevniStorage.java
@@ -45,7 +45,6 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.pig.LoadPushDown;
 import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigFileInputFormat;
 import org.apache.pig.data.Tuple;
-import org.apache.pig.impl.util.Utils;
 import org.apache.pig.impl.util.avro.AvroRecordWriter;
 import org.apache.pig.impl.util.avro.AvroStorageDataConversionUtilities;
 import org.apache.trevni.ColumnFileMetaData;
@@ -110,7 +109,7 @@ public class TrevniStorage extends AvroStorage implements LoadPushDown{
         List<FileStatus> results = Lists.newArrayList();
         job.getConfiguration().setBoolean("mapred.input.dir.recursive", true);
         for (FileStatus file : super.listStatus(job)) {
-          if (Utils.VISIBLE_FILES.accept(file.getPath())) {
+          if (VISIBLE_FILES.accept(file.getPath())) {
             results.add(file);
           }
         }
@@ -309,7 +308,7 @@ public class TrevniStorage extends AvroStorage implements LoadPushDown{
     ArrayList<FileStatus> statusList = new ArrayList<FileStatus>();
     FileSystem fs = FileSystem.get(p[0].toUri(), job.getConfiguration());
     for (Path temp : p) {
-      for (FileStatus tempf : fs.globStatus(temp, Utils.VISIBLE_FILES)) {
+      for (FileStatus tempf : fs.globStatus(temp, VISIBLE_FILES)) {
         statusList.add(tempf);
       }
     }
@@ -324,7 +323,7 @@ public class TrevniStorage extends AvroStorage implements LoadPushDown{
       throw new IOException("No path matches pattern " + p.toString());
     }
 
-    Path filePath = Utils.depthFirstSearchForFile(statusArray, fs);
+    Path filePath = depthFirstSearchForFile(statusArray, fs);
     
     if (filePath == null) {
       throw new IOException("No path matches pattern " + p.toString());
