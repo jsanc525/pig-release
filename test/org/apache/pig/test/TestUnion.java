8ac43585d5210c4ca1bb84cee4f8be9c71ba9943
Daniel Dai <daijy@hortonworks.com>
2014-10-27 10:17:33 -0700

    BUG-25636: Windows Tez: grunt_13, grunt_14 , grunt_15 fails with Encountered IOException

--- test/org/apache/pig/test/TestUnion.java
+++ test/org/apache/pig/test/TestUnion.java
@@ -95,7 +95,7 @@ public class TestUnion {
         POLoad ld1 = GenPhyOp.topLoadOp();
         String curDir = System.getProperty("user.dir");
         String inpDir = curDir + File.separatorChar + "test/org/apache/pig/test/data/InputFiles/";
-        FileSpec fSpec = new FileSpec(Util.generateURI(Util.encodeEscape(inpDir + "passwd"), pc), new FuncSpec(PigStorage.class.getName() , new String[]{":"}));
+        FileSpec fSpec = new FileSpec(Util.generateURI((inpDir + "passwd"), pc), new FuncSpec(PigStorage.class.getName() , new String[]{":"}));
         ld1.setLFile(fSpec);
 
         POLoad ld2 = GenPhyOp.topLoadOp();
