90324b2f7a9e6fc79a8e66a05b7e86df24a45507
Daniel Dai <daijy@hortonworks.com>
2015-04-10 18:52:12 -0700

    Fix HBase unit test failures

--- test/org/apache/pig/test/TestHBaseStorage.java
+++ test/org/apache/pig/test/TestHBaseStorage.java
@@ -1279,7 +1279,7 @@ public class TestHBaseStorage {
         Object key = "somekey";
         byte type = DataType.CHARARRAY;
         Assert.assertTrue(hbaseStorage.createPut(key, type).getDurability() == Durability.SKIP_WAL);
-        Assert.assertFalse(hbaseStorage.createDelete(key, type, System.currentTimeMillis()).getDurability() == Durability.SKIP_WAL);
+        Assert.assertTrue(hbaseStorage.createDelete(key, type, System.currentTimeMillis()).getDurability() == Durability.SKIP_WAL);
     }
 
     /**
@@ -1294,7 +1294,7 @@ public class TestHBaseStorage {
         Object key = "somekey";
         byte type = DataType.CHARARRAY;
         Assert.assertFalse(hbaseStorage.createPut(key, type).getDurability() == Durability.SKIP_WAL);
-        Assert.assertTrue(hbaseStorage.createDelete(key, type, System.currentTimeMillis()).getDurability() == Durability.SKIP_WAL);
+        Assert.assertFalse(hbaseStorage.createDelete(key, type, System.currentTimeMillis()).getDurability() == Durability.SKIP_WAL);
     }
 
     /**
