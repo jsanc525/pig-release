b5a9ef2de585922bbae73c3a48170db0cf1bb211
Daniel Dai <daijy@hortonworks.com>
2015-06-17 09:20:32 -0700

    BUG-39098: Umbrella Jira for Pig Windows UT

--- test/org/apache/pig/tez/TestTezAutoParallelism.java
+++ test/org/apache/pig/tez/TestTezAutoParallelism.java
@@ -274,7 +274,7 @@ public class TestTezAutoParallelism {
         // skewed join parallelism take the initial setting, since the join vertex has a broadcast(scalar) dependency,
         // which prevent it changing parallelism
         pigServer.getPigContext().getProperties().setProperty(PigConfiguration.PIG_NO_SPLIT_COMBINATION, "true");
-        pigServer.getPigContext().getProperties().setProperty(MRConfiguration.MAX_SPLIT_SIZE, "3000");
+        pigServer.getPigContext().getProperties().setProperty(MRConfiguration.MAX_SPLIT_SIZE, "4000");
         pigServer.getPigContext().getProperties().setProperty(InputSizeReducerEstimator.BYTES_PER_REDUCER_PARAM, "40000");
         pigServer.registerQuery("A = load '" + INPUT_FILE1 + "' as (name:chararray, age:int);");
         pigServer.registerQuery("B = load '" + INPUT_FILE2 + "' as (name:chararray, gender:chararray);");
