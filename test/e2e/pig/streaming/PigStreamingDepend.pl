dfa37d60d7aa8c205650fcc10c095f3e5f96ba2c
Daniel Dai <daijyc@gmail.com>
2018-12-14 17:10:18 -0800

    Revert "BUG-112168 PIG-5366: Enable PigStreamingDepend to load from current directory in newer Perl versions"
    This reverts commit 71b243952404744e1b182261747e9a937e44baee.

--- test/e2e/pig/streaming/PigStreamingDepend.pl
+++ test/e2e/pig/streaming/PigStreamingDepend.pl
@@ -16,9 +16,6 @@
 #  See the License for the specific language governing permissions and                 
 #  limitations under the License.                                                      
                                                                                        
-BEGIN {
-    push @INC, '.';
-}
 
 use strict;
 use PigStreamingModule;
